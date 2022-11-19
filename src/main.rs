dd25888c6f03a7e515ad9e5c12dd113ff7fda796
Mike English <mike.english@gmail.com>
2022-11-19 12:01:23 -0500

    Build reader/writer in listen thread

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,39 @@
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::net::{TcpListener, TcpStream};
-use std::thread;
 use std::sync::mpsc::{channel, Sender};
+use std::thread;
 
-fn handle_client(mut stream: TcpStream, tx: Sender<String>) {
+fn handle_client(
+    mut reader: BufReader<TcpStream>,
+    mut writer: BufWriter<TcpStream>,
+    tx: Sender<String>,
+) {
     let mut name = String::new();
-    let _ = stream.write(b"Name: ");
-    let mut input = BufReader::new(stream.try_clone().unwrap());
-    let mut output = BufWriter::new(stream);
-    let _ = input.read_line(&mut name);
+    let _ = writer.write(b"Name: ");
+    writer.flush();
+    let _ = reader.read_line(&mut name);
     let len = name.trim_matches(&['\r', '\n'][..]).len();
     name.truncate(len);
 
     println!("Thread says: {} has joined.", name);
     let greeting = format!("Hello {}\n", name);
     tx.send(name).unwrap();
-    let _ = output.write(greeting.as_bytes());
+    let _ = writer.write(greeting.as_bytes());
 }
 
-fn listen(sock: TcpListener, tx: Sender<String>){
+fn listen(sock: TcpListener, tx: Sender<String>) {
     for stream in sock.incoming() {
         let next_tx = tx.clone();
         match stream {
             Ok(stream) => {
-                thread::spawn(move || handle_client(stream, next_tx));
+                thread::spawn(move || {
+                    println!("in the thread, but not yet the fn?");
+                    let reader = BufReader::new(stream.try_clone().unwrap());
+                    let writer = BufWriter::new(stream);
+                    handle_client(reader, writer, next_tx)
+                });
             }
-            Err(e) => { /* connection failed */ }
+            Err(_e) => { /* connection failed */ }
         }
     }
     drop(sock);
