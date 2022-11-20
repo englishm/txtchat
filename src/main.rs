22dad13fceb059a8b59a71c78b5f92bf70327076
Mike English <mike.english@gmail.com>
2022-11-19 19:21:19 -0500

    Work towards main chat loop for each session

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::net::{TcpListener, TcpStream};
-use std::sync::mpsc::{channel, Sender};
+use std::sync::mpsc::{channel, Sender, Receiver};
 use std::thread;
 
-enum Message {
+enum ClientMessage {
     Session {
         name: String,
         reader: BufReader<TcpStream>,
         writer: BufWriter<TcpStream>,
     },
 }
+enum ServerMessage {
+    Output {
+        message: String,
+    },
+}
 
 fn handle_client(
     mut reader: BufReader<TcpStream>,
     mut writer: BufWriter<TcpStream>,
-    tx: Sender<Message>,
+    tx: Sender<ClientMessage>,
 ) {
     // Get user's name (later: do some kind of auth)
     let mut name = String::new();
@@ -31,7 +36,7 @@ fn handle_client(
     writer.flush().unwrap();
 
     // Initialize session
-    let session = Message::Session {
+    let session = ClientMessage::Session {
         name: name,
         reader: reader,
         writer: writer,
@@ -40,7 +45,7 @@ fn handle_client(
     tx.send(session).unwrap();
 }
 
-fn listen(sock: TcpListener, tx: Sender<Message>) {
+fn listen(sock: TcpListener, tx: Sender<ClientMessage>) {
     for stream in sock.incoming() {
         let next_tx = tx.clone();
         match stream {
@@ -58,6 +63,10 @@ fn listen(sock: TcpListener, tx: Sender<Message>) {
     drop(sock);
 }
 
+fn chat_loop(session: ClientMessage, rx: Receiver<ServerMessage>){
+            unimplemented!("Do loops!")
+}
+
 fn main() {
     let (tx, rx) = channel();
     let listener = TcpListener::bind("127.0.0.1:2323").unwrap();
@@ -66,15 +75,17 @@ fn main() {
 
     for session in rx {
         match session {
-            Message::Session {
+            ClientMessage::Session {
                 name,
-                reader: _,
-                writer: _,
+                reader,
+                writer,
             } => {
                 println!("main says: {} has joined.", name);
+                let (tx2, rx2) = channel();
+                chat_loop(ClientMessage::Session{name, reader, writer}, rx2);
             }
             _ => {
-                unimplemented!("handle non-session messages")
+                unimplemented!("Error appropriately on unexpected non-Session first messages")
             }
         }
     }
